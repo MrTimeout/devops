63c229d84d8589be94f0f4384905e3a1845d2503
MrTimeout <estonoesmiputocorreo@gmail.com>
2022-02-21 13:56:31 +0100

    Adding variables lab

---
+++ nginx/Variables/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl --silent http://localhost:80/inspect?name=ivan&other=pedro
+
+curl --silent http://localhost:80/inspect?name=kendelt
+
+curl --silent http://localhost:80/is_weekend
